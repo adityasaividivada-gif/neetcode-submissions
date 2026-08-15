f79070723671b40fd2cbe780d663aab3ee2d7c72
adityasaividivada-gif <adityasaividivada@gmail.com>
2026-08-15 14:46:10 +0530

    Add: sql-alter-table - submission-1

---
+++ SQL/sql-alter-table/submission-1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE books (
+  id INTEGER,
+  title TEXT,
+  author TEXT
+);
+-- Do not modify above this line --
+
+alter table books add column published_year INTEGER;
+
+ALTER TABLE BOOKS RENAME COLUMN ID TO ISBN;
+
+ALTER TABLE BOOKS DROP COLUMN AUTHOR;
+
+
+
+
+-- Do not modify below this line --
+SELECT column_name, data_type, column_default
+FROM information_schema.columns
+WHERE table_name = 'books'
+ORDER BY column_name;
