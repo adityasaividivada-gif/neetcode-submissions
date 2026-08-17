dad5ee62347aea760cb8e1a49bd1880fe4243001
adityasaividivada-gif <adityasaividivada@gmail.com>
2026-08-17 18:21:16 +0530

    Add: sql-not-null-constraint - submission-2

---
+++ SQL/sql-not-null-constraint/submission-2.sql
@@ -0,0 +1,15 @@
+CREATE TABLE PRODUCTS (
+    NAME TEXT NOT NULL DEFAULT 'Unknown',
+    price INTEGER not null,
+    quantity integer default 0
+);
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT column_name, data_type, is_nullable, column_default
+FROM information_schema.columns
+WHERE table_name = 'products';
