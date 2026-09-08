72ea4ad60d1354c18a76156b178bca61288297ec
adityasaividivada-gif <adityasaividivada@gmail.com>
2026-09-08 15:50:04 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 1.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Hello, World!"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
