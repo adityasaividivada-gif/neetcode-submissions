c6694cb0cbdca12e05228c4091a45433da715f4f
adityasaividivada-gif <adityasaividivada@gmail.com>
2026-09-13 18:41:24 +0530

    Add: remove-linked-list-elements - submission-0

---
+++ Data Structures & Algorithms/remove-linked-list-elements/submission-0.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        
+        dummy=ListNode(0)
+        dummy.next=head
+
+        prev=dummy
+        curr=head
+        while(curr):
+            nextNode=curr.next
+            if curr.val==val:
+                prev.next=nextNode
+                curr=nextNode
+            else:
+                prev=curr
+                curr=nextNode
+        return dummy.next
+
+
+            
