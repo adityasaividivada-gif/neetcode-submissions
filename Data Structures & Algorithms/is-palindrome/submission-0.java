6a2857bc340d7d9ae1229a1a3887d53a0dd70168
adityasaividivada-gif <adityasaividivada@gmail.com>
2026-09-21 09:05:13 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        s=s.replaceAll("[^a-zA-Z0-9]","");
+        int left=0;
+        int right=s.length()-1;
+        System.out.println(s);
+        while(left<=right)
+        {
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right)))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+
+        
+    }
+}
